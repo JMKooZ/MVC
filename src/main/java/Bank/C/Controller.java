d6bae70abb0df91ab9f8581d4ec9fc9c8b7cd099
JMKooZ <ck03412@naver.com>
2023-04-21 15:51:05 +0900

    뱅크 연습
    계좌송금 추가

--- src/main/java/Bank/C/Controller.java
+++ src/main/java/Bank/C/Controller.java
@@ -1,9 +1,14 @@
 package main.java.Bank.C;
 
 import main.java.Bank.M.CustomerDao;
+import main.java.Bank.V.MenuView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Component
 public class Controller {
@@ -16,10 +21,12 @@ public class Controller {
     infoService infoService;
     deleteService deleteService;
     changeService changeService;
-@Autowired
+    AccountTransService accountTransService;
+
+    @Autowired
     public Controller(LoginService loginService, InsertService insertService, CustomerDao customerDao,
-                      depositService depositService, withdrawMoney withdrawMoney,infoService infoService, deleteService deleteService,
-                      changeService changeService) {
+                      depositService depositService, withdrawMoney withdrawMoney, infoService infoService, deleteService deleteService,
+                      changeService changeService, AccountTransService accountTransService) {
         this.loginService = loginService;
         this.insertService = insertService;
         this.customerDao = customerDao;
@@ -28,15 +35,18 @@ public class Controller {
         this.infoService = infoService;
         this.deleteService = deleteService;
         this.changeService = changeService;
+        this.accountTransService = accountTransService;
     }
 
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
     public String login(String id, String pw) {
-        LoginService loginService = (LoginService)context.getBean("loginService", LoginService.class);
+        LoginService loginService = (LoginService) context.getBean("loginService", LoginService.class);
         try {
-            if(loginService.login_service(id, pw).equals("s")) {
+            if (loginService.login_service(id, pw).equals("s")) {
                 loginService.login_service(id, pw);
                 return "success";
-            }else{
+            } else {
                 return "f";
             }
         } catch (Exception e) {
@@ -46,7 +56,7 @@ public class Controller {
     }
 
     public String insert(String id, String pw, String name, int birth) {
-        InsertService insertService = (InsertService)context.getBean("insertService", InsertService.class);
+        InsertService insertService = (InsertService) context.getBean("insertService", InsertService.class);
         try {
             if (id.equals(this.customerDao.selectCustomer(id).getId())) {
                 System.out.println("중복된 아이디입니다.");
@@ -60,23 +70,26 @@ public class Controller {
         }
     }
 
-    public String depositMoney(String id, int money) {
-        depositService depositService = (depositService)context.getBean("depositService", depositService.class);
-        try {
+    public void depositMoney(String id, String money) {
+        depositService depositService = (depositService) context.getBean("depositService", depositService.class);
+        if (pattern(money)) {
             depositService.deposit(id, money);
-            return "success";
-        } catch (NumberFormatException var5) {
-            System.out.println("입금하실 금액을 입력해주세요");
-            return "fail";
+            System.out.println(money + "원 입금에 성공하였습니다.");
+        } else {
+            System.out.println("입력 값을 확인해주세요");
         }
     }
 
-    public void withdrawMoney(String id, int money) {
-        withdrawMoney withdrawMoney = (withdrawMoney)context.getBean("withdrawMoney", withdrawMoney.class);
-        if( withdrawMoney.withdrawMoney(id, money).equals("fail")) {
-            System.out.println("잔고보다 출금액이 많을수 없다.");
-        }else{
-            System.out.println("출금에 성공했다.");
+    public void withdrawMoney(String id, String money) {
+        withdrawMoney withdrawMoney = (withdrawMoney) context.getBean("withdrawMoney", withdrawMoney.class);
+        if (pattern(money)) {
+            if (withdrawMoney.withdrawMoney(id, money).equals("fail")) {
+                System.out.println("잔고보다 출금액이 많을수 없다.");
+            } else {
+                System.out.println(money + "원 출금에 성공했다.");
+            }
+        } else {
+            System.out.println("입력 값을 확인해주세요");
         }
     }
 
@@ -84,23 +97,61 @@ public class Controller {
         System.out.println(this.customerDao.selectCustomer(id).getMoney());
     }
 
-    public void information(String id){
+    public void information(String id) {
         infoService infoService = context.getBean("infoService", infoService.class);
         infoService.info(id);
     }
-    public void delete(String id){
+
+    public void delete(String id) {
         customerDao.deleteCustomer(id);
     }
-    public String change(String id, String pw ,String newPw){
-        changeService changeService = context.getBean("change",changeService.class);
-        if(!pw.equals(customerDao.selectCustomer(id).getPw())) {
+
+    public String change(String id, String pw, String newPw) {
+        changeService changeService = context.getBean("change", changeService.class);
+        if (!pw.equals(customerDao.selectCustomer(id).getPw())) {
             System.out.println("비밀번호가 일치하지 않습니다.");
             return "fail";
-        }else {
+        } else {
             changeService.change(id, newPw);
             System.out.println("변경완료했습니다.");
             System.out.println("다시 로그인 해주세요.");
             return "success";
+        }
+    }
+
+    public void accountTrans(String id,String account) throws IOException {
+        AccountTransService accountTransService = context.getBean("accountTransService", AccountTransService.class);
+        try {
+            if (account.equals(customerDao.accountCustomer(account).getAccount())) {
+                System.out.println(customerDao.accountCustomer(account).getId() + "님이 맞습니까?");
+                System.out.println("입금하실 금액을 입력해주세요");
+                String money = reader.readLine();
+                if (pattern(money)) {
+                    if (customerDao.selectCustomer(id).getMoney() < Integer.parseInt(money)) {
+                        System.out.println("잔액이 부족합니다.");
+                    } else {
+                        customerDao.selectCustomer(id).withdrawMoney(Integer.parseInt(money));
+                        customerDao.accountCustomer(account).depositMoney(Integer.parseInt(money));
+                        System.out.println("송금에 성공했습니다.");
+                    }
+                } else {
+                    System.out.println("입력 값을 확인해주세요");
+                }
+            } else {
+                System.out.println("계좌가 일치하지 않습니다.");
+            }
+        } catch (NullPointerException e) {
+            System.out.println("입력하신 계좌는 없는 계좌번호입니다.");
+        }
+//        accountTransService.transfer(id,account);
+    }
+
+    public static boolean pattern(String money) {
+        String reg = "^[1-9]\\d*$";
+        if (money.matches(reg)) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
